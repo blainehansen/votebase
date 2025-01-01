621bea0b7d7094ea27510368543d167e1ce674a2
Blaine Hansen <faichenshing@gmail.com>
2025-01-01 16:46:11 -0700

    figuring out the api of deno core, and how I want to use it

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,38 @@
-// Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
-//!  This example shows you how to define ops in Rust and then call them from
-//!  JavaScript.
+#[macro_use] extern crate rocket;
+
+#[derive(Debug)]
+struct PathList<'r>(Vec<&'r str>);
+
+impl <'r> rocket::request::FromSegments<'r> for PathList<'r> {
+	type Error = std::convert::Infallible;
+
+	fn from_segments(raw_segments: rocket::http::uri::Segments<'r, rocket::http::uri::fmt::Path>) -> Result<Self, Self::Error> {
+		Ok(PathList(raw_segments.into_iter().collect()))
+	}
+}
+
+#[catch(default)]
+fn default_catcher(status: rocket::http::Status, req: &rocket::Request<'_>) -> rocket::response::status::Custom<String> {
+	let msg = format!("{} ({})", status, req.uri());
+	rocket::response::status::Custom(status, msg)
+}
+
+#[launch]
+fn rocket() -> _ {
+	rocket::build()
+		.configure(rocket::Config {
+			port: 8080,
+			..Default::default()
+		})
+		.register("/", catchers![default_catcher])
+		.mount("/", routes![execute_action, execute_view])
+}
+
+#[post("/action/<path..>?<_arg>")]
+fn execute_action(path: PathList<'_>, _arg: Option<&str>) -> String {
+	format!("{:?}", path.0)
+}
+
 
 /// An op for summing an array of numbers. The op-layer automatically
 /// deserializes inputs and serializes the returned Result & value.
@@ -12,18 +44,21 @@ fn op_sum(#[serde] nums: Vec<f64>) -> Result<f64, deno_core::error::AnyError> {
 	Ok(sum)
 }
 
-fn main() {
-	// Build a deno_core::Extension providing custom ops
-	const DECL: deno_core::OpDecl = op_sum();
-	let ext = deno_core::Extension {
-		name: "my_ext",
-		ops: std::borrow::Cow::Borrowed(&[DECL]),
-		..Default::default()
-	};
 
+deno_core::extension!(
+	my_ext,
+	ops = [
+		op_sum,
+	],
+);
+// esm_entry_point = "ext:runjs/runtime.js",
+// esm = [dir "src", "runtime.js"],
+
+#[get("/view/<_path..>?<_query>")]
+fn execute_view(_path: PathList<'_>, _query: Option<&str>) {
 	// Initialize a runtime instance
 	let mut runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions {
-		extensions: vec![ext],
+		extensions: vec![my_ext::init_ops()],
 		// create_params: Some(v8::Isolate::create_params()
 		// 	.heap_limits(initial, max)
 		// ),
