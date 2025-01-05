46f2a9b11ec64f349619fc27a4232ac7ad686b35
Blaine Hansen <faichenshing@gmail.com>
2025-01-04 17:47:53 -0700

    got called functions working

--- src/main.rs
+++ src/main.rs
@@ -1,117 +1,3 @@
-// #[macro_use] extern crate rocket;
-
-// #[derive(Debug)]
-// struct PathList<'r>(Vec<&'r str>);
-
-// impl <'r> rocket::request::FromSegments<'r> for PathList<'r> {
-// 	type Error = std::convert::Infallible;
-
-// 	fn from_segments(raw_segments: rocket::http::uri::Segments<'r, rocket::http::uri::fmt::Path>) -> Result<Self, Self::Error> {
-// 		Ok(PathList(raw_segments.into_iter().collect()))
-// 	}
-// }
-
-// #[catch(default)]
-// fn default_catcher(status: rocket::http::Status, req: &rocket::Request<'_>) -> rocket::response::status::Custom<String> {
-// 	let msg = format!("{} ({})", status, req.uri());
-// 	rocket::response::status::Custom(status, msg)
-// }
-
-// #[launch]
-// fn rocket() -> _ {
-// 	rocket::build()
-// 		.configure(rocket::Config {
-// 			port: 8080,
-// 			..Default::default()
-// 		})
-// 		.register("/", catchers![default_catcher])
-// 		.mount("/", routes![execute_action, execute_view])
-// }
-
-// #[post("/action/<path..>?<_arg>")]
-// fn execute_action(path: PathList<'_>, _arg: Option<&str>) -> String {
-// 	format!("{:?}", path.0)
-// }
-
-// esm_entry_point = "ext:runjs/runtime.js",
-// esm = [dir "src", "runtime.js"],
-
-// #[get("/view/<_path..>?<_query>")]
-// fn execute_view(_path: PathList<'_>, _query: Option<&str>) {
-// 	// Initialize a runtime instance
-// 	let mut runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions {
-// 		extensions: vec![my_ext::init_ops()],
-// 		// create_params: Some(v8::Isolate::create_params()
-// 		// 	.heap_limits(initial, max)
-// 		// ),
-// 		..Default::default()
-// 	});
-
-// 	// Now we see how to invoke the op we just defined. The runtime automatically
-// 	// contains a Deno.core object with several functions for interacting with it.
-// 	// You can find its definition in core.js.
-// 	runtime
-// 		.execute_script(
-// 			"<usage>",
-// 			r#"
-// // Print helper function, calling Deno.core.print()
-// function print(value) {
-// 	Deno.core.print(value.toString()+"\n");
-// }
-
-// const arr = [1, 2, 3];
-// print("The sum of");
-// print(arr);
-// print("is");
-// print(Deno.core.ops.op_sum(arr));
-
-// // And incorrect usage
-// try {
-// 	print(Deno.core.ops.op_sum(0));
-// } catch(e) {
-// 	print('Exception:');
-// 	print(e);
-// }
-// "#,
-// 		)
-// 		.unwrap();
-// }
-
-#[deno_core::op2(async)]
-#[string]
-async fn op_read_file(#[string] path: String) -> Result<String, deno_core::error::AnyError> {
-	let contents = tokio::fs::read_to_string(path).await?;
-	Ok(contents)
-}
-
-#[deno_core::op2(async)]
-async fn op_write_file(
-	#[string] path: String,
-	#[string] contents: String,
-) -> Result<(), deno_core::error::AnyError> {
-	tokio::fs::write(path, contents).await?;
-	Ok(())
-}
-
-#[deno_core::op2(async)]
-#[string]
-async fn op_fetch(#[string] url: String) -> Result<String, deno_core::error::AnyError> {
-	let body = reqwest::get(url).await?.text().await?;
-	Ok(body)
-}
-
-#[deno_core::op2(async)]
-async fn op_set_timeout(delay: f64) -> Result<(), deno_core::error::AnyError> {
-	tokio::time::sleep(std::time::Duration::from_millis(delay as u64)).await;
-	Ok(())
-}
-
-#[deno_core::op2(fast)]
-fn op_remove_file(#[string] path: String) -> Result<(), deno_core::error::AnyError> {
-	std::fs::remove_file(path)?;
-	Ok(())
-}
-
 struct TsModuleLoader;
 
 impl deno_core::ModuleLoader for TsModuleLoader {
@@ -120,7 +6,7 @@ impl deno_core::ModuleLoader for TsModuleLoader {
 		specifier: &str,
 		referrer: &str,
 		_kind: deno_core::ResolutionKind,
-	) -> Result<deno_core::ModuleSpecifier, deno_core::error::AnyError> {
+	) -> Result<deno_core::ModuleSpecifier, DenoError> {
 		deno_core::resolve_import(specifier, referrer).map_err(|e| e.into())
 	}
 
@@ -185,51 +71,172 @@ impl deno_core::ModuleLoader for TsModuleLoader {
 	}
 }
 
+
+
+// #[macro_use] extern crate rocket;
+
+// #[derive(Debug)]
+// struct PathList<'r>(Vec<&'r str>);
+
+// impl <'r> rocket::request::FromSegments<'r> for PathList<'r> {
+// 	type Error = std::convert::Infallible;
+
+// 	fn from_segments(raw_segments: rocket::http::uri::Segments<'r, rocket::http::uri::fmt::Path>) -> Result<Self, Self::Error> {
+// 		Ok(PathList(raw_segments.into_iter().collect()))
+// 	}
+// }
+
+// #[catch(default)]
+// fn default_catcher(status: rocket::http::Status, req: &rocket::Request<'_>) -> rocket::response::status::Custom<String> {
+// 	let msg = format!("{} ({})", status, req.uri());
+// 	rocket::response::status::Custom(status, msg)
+// }
+
+// #[launch]
+// fn rocket() -> _ {
+// 	rocket::build()
+// 		.configure(rocket::Config {
+// 			port: 8080,
+// 			..Default::default()
+// 		})
+// 		.register("/", catchers![default_catcher])
+// 		.mount("/", routes![execute_action, execute_view])
+// }
+
+// #[post("/action/<path..>?<_arg>")]
+// fn execute_action(path: PathList<'_>, _arg: Option<&str>) -> String {
+// 	format!("{:?}", path.0)
+// }
+
+// esm_entry_point = "ext:votebase/runtime.js",
+// esm = [dir "src", "runtime.js"],
+
+// #[get("/view/<_path..>?<_query>")]
+// fn execute_view(_path: PathList<'_>, _query: Option<&str>) {
+// 	// Initialize a runtime instance
+// 	let mut runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions {
+// 		extensions: vec![my_ext::init_ops()],
+// 		// create_params: Some(v8::Isolate::create_params()
+// 		// 	.heap_limits(initial, max)
+// 		// ),
+// 		..Default::default()
+// 	});
+
+// 	// Now we see how to invoke the op we just defined. The runtime automatically
+// 	// contains a Deno.core object with several functions for interacting with it.
+// 	// You can find its definition in core.js.
+// 	runtime
+// 		.execute_script(
+// 			"<usage>",
+// 			r#"
+// // Print helper function, calling Deno.core.print()
+// function print(value) {
+// 	Deno.core.print(value.toString()+"\n");
+// }
+
+// const arr = [1, 2, 3];
+// print("The sum of");
+// print(arr);
+// print("is");
+// print(Deno.core.ops.op_sum(arr));
+
+// // And incorrect usage
+// try {
+// 	print(Deno.core.ops.op_sum(0));
+// } catch(e) {
+// 	print('Exception:');
+// 	print(e);
+// }
+// "#,
+// 		)
+// 		.unwrap();
+// }
+
+use deno_core::v8;
+
+type DenoError = deno_core::error::AnyError;
+
+#[deno_core::op2(async)]
+#[string]
+async fn op_fetch(#[string] url: String) -> Result<String, DenoError> {
+	let body = reqwest::get(url).await?.text().await?;
+	Ok(body)
+}
+
+#[deno_core::op2(async)]
+async fn op_set_timeout(delay: f64) -> Result<(), DenoError> {
+	tokio::time::sleep(std::time::Duration::from_millis(delay as u64)).await;
+	Ok(())
+}
+
+
 static RUNTIME_SNAPSHOT: &[u8] =
-	include_bytes!(concat!(env!("OUT_DIR"), "/RUNJS_SNAPSHOT.bin"));
+	include_bytes!(concat!(env!("OUT_DIR"), "/VOTEBASE_SNAPSHOT.bin"));
+
+type FunctionMap = std::collections::HashMap<String, v8::Global<v8::Function>>;
+
+
+// https://github.com/denoland/deno_core/issues/515
+// https://discord.com/channels/684898665143206084/1022163295895027722/threads/1201661871959310346
+// https://gist.github.com/alshdavid/c9e5bc0d794e3ec9dba6afaa689b704e#file-main-rs-L51
+
+// https://discord.com/channels/684898665143206084/1022163295895027722/threads/1074150763460313128
+
+#[deno_core::op2]
+fn op_register_func(
+	#[state] function_state: &mut FunctionMap,
+	#[string] key: String,
+	#[global] func: v8::Global<v8::Function>,
+) {
+	function_state.insert(key, func);
+}
 
 deno_core::extension!(
-	runjs,
+	votebase,
 	ops = [
-		op_read_file,
-		op_write_file,
-		op_remove_file,
 		op_fetch,
 		op_set_timeout,
-	]
+		op_register_func,
+	],
+	state = |state: &mut deno_core::OpState| {
+		state.put(std::collections::HashMap::<String, v8::Global<v8::Function>>::new());
+	},
 );
 
-async fn run_js(file_path: &str) -> Result<(), deno_core::error::AnyError> {
-	let current_dir = std::env::current_dir()?;
-
-	let side_module = deno_core::resolve_path(file_path, &current_dir)?;
+async fn run_function(constitution_code: String, function_name: &str) -> Result<(), DenoError> {
 	let mut js_runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions {
-		module_loader: Some(std::rc::Rc::new(TsModuleLoader)),
+		// extension_transpiler: Some(),
+		module_loader: None,
 		startup_snapshot: Some(RUNTIME_SNAPSHOT),
-		extensions: vec![runjs::init_ops()],
+		extensions: vec![votebase::init_ops()],
 		..Default::default()
 	});
 
-	let mod_id = js_runtime.load_side_es_module(&side_module).await?;
+	let specifier = deno_core::resolve_url("votebase:<main>")?;
+	let mod_id = js_runtime.load_main_es_module_from_code(&specifier, constitution_code).await?;
 	let result = js_runtime.mod_evaluate(mod_id);
 	js_runtime.run_event_loop(Default::default()).await?;
 	result.await?;
 
-	let function_name = "yo";
-	let module_name = "example";
-	let raw_input = "4";
-	let main_code = format!("import {{{function_name}}} from './{module_name}.ts'; {function_name}({raw_input})");
-	let main_module = deno_core::resolve_path("<main>", &current_dir)?;
-	let mod_id = js_runtime.load_main_es_module_from_code(&main_module, main_code).await?;
-	let result = js_runtime.mod_evaluate(mod_id);
-	js_runtime.run_event_loop(Default::default()).await?;
-	result.await?;
+	let state: FunctionMap = js_runtime.op_state().borrow_mut().take();
+	let function = state.get(function_name).unwrap();
 
-	// https://github.com/denoland/deno_core/issues/515
-	// https://discord.com/channels/684898665143206084/1022163295895027722/threads/1201661871959310346
-	// https://gist.github.com/alshdavid/c9e5bc0d794e3ec9dba6afaa689b704e#file-main-rs-L51
+	let arg = {
+		let mut scope = js_runtime.handle_scope();
+		let arg = deno_core::serde_v8::to_v8(&mut scope, "hello arg")?;
+		v8::Global::new(&mut scope, arg)
+	};
 
-	// https://discord.com/channels/684898665143206084/1022163295895027722/threads/1074150763460313128
+	let call = js_runtime.call_with_args(&function, &[arg]);
+
+	let function_ret = js_runtime
+		.with_event_loop_promise(call, deno_core::PollEventLoopOptions::default())
+		.await?;
+
+	let mut scope = js_runtime.handle_scope();
+	let function_ret = v8::Local::new(&mut scope, function_ret);
+	let function_ret: bool = deno_core::serde_v8::from_v8(&mut scope, function_ret)?;
+	dbg!(function_ret);
 
 	Ok(())
 }
@@ -240,7 +247,17 @@ fn main() {
 		.build()
 		.unwrap();
 
-	if let Err(error) = runtime.block_on(run_js("example.ts")) {
+	if let Err(error) = runtime.block_on(run_function(CONSTITUTION_CODE.to_string(), FUNCTION_NAME)) {
 		eprintln!("error: {error}");
 	}
 }
+
+const CONSTITUTION_CODE: &'static str = r#"
+console.log("wassup")
+
+votebase.reg("hello", (arg) => {
+	console.log(arg)
+	return arg.startsWith("hello")
+})
+"#;
+const FUNCTION_NAME: &'static str = "hello";
--- src/runtime.js
+++ src/runtime.js
@@ -1,33 +1,27 @@
-const { core } = Deno;
+const { core } = Deno
 
 function argsToMessage(...args) {
-	return args.map((arg) => JSON.stringify(arg)).join(" ");
+	return args.map((arg) => JSON.stringify(arg)).join(" ")
 }
 
 globalThis.console = {
 	log: (...args) => {
-		core.print(`[out]: ${argsToMessage(...args)}\n`, false);
+		core.print(`[out]: ${argsToMessage(...args)}\n`, false)
 	},
 	error: (...args) => {
-		core.print(`[err]: ${argsToMessage(...args)}\n`, true);
+		core.print(`[err]: ${argsToMessage(...args)}\n`, true)
 	},
-};
+}
 
-globalThis.runjs = {
-	readFile: (path) => {
-		return core.ops.op_read_file(path);
+globalThis.votebase = {
+	fetch: url => {
+		return core.ops.op_fetch(url)
 	},
-	writeFile: (path, contents) => {
-		return core.ops.op_write_file(path, contents);
+	reg: (name, func) => {
+		core.ops.op_register_func(name, func)
 	},
-	removeFile: (path) => {
-		return core.ops.op_remove_file(path);
-	},
-	fetch: async (url) => {
-		return core.ops.op_fetch(url);
-	},
-};
+}
 
 globalThis.setTimeout = async (callback, delay) => {
-	core.ops.op_set_timeout(delay).then(callback);
-};
+	core.ops.op_set_timeout(delay).then(callback)
+}
