723e9deaa44cbf339576d34a833ed6261b62b8cf
Blaine Hansen <faichenshing@gmail.com>
2025-01-18 17:18:28 -0700

    dev script

--- dev.sh
+++ dev.sh
@@ -1,1 +1,9 @@
-curl -v "http://localhost:8080/view/some?hmmm=y"
+# curl -v "http://localhost:8080/view/some?hmmm=y"
+
+psql
+
+dev_db
+dev_user
+dev_password
+
+# format the above psql command correctly ai!
